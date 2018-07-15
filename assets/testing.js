bf842bbcb4095ab45624c8bd3c9cf453b9683e07
Ash <asha.mohamed.28@gmail.com>
2018-07-14 22:10:57 -0400

    getting total price of recipe ingredients

--- assets/testing.js
+++ assets/testing.js
@@ -11,12 +11,14 @@ var image_url = "";
 var ingredientLines = "";
 var ingredientList = "";
 var ingredient = "";
-var ingredientName = ""; 
-var price = ""; 
+var ingredientName = "";
+var price = "";
+var totalPrice = [0, 0, 0];
+var arrayPrice = [];
 
 
 // =========================
-// SET UP METHODS: EDAMAM
+// CALLING THE FIRST
 // ========================
 $(".recipe").on("click", function () {
     // using term 'this' to identify specific buttons
@@ -24,7 +26,7 @@ $(".recipe").on("click", function () {
     console.log(x);
 
     // SET UP VARIABLES FOR AJAX CALL 
-    queryItem = 'chicken';
+
     var queryURL = "https://api.edamam.com/search?q=" + x + "&app_id=" + appId + "&app_key=" + appKey;
     //testing
     console.log(queryURL);
@@ -38,142 +40,102 @@ $(".recipe").on("click", function () {
         console.log("Recipe Button Clicked!")
         //recipes is results.hits which is an array of recipes
         var recipes = results.hits;
-        //for loop to iterate through recipes array
+
         for (var i = 0; i < 3; i++) {
-            //recipes[i] (each recipe) is an object
-            var recipe = recipes[i].recipe;
-            var title = recipe.label;
-            var image_url = recipe.image;
-
-            //ingredients is an array
-            var ingredients = recipe.ingredientLines;
-            var ingredientList = $('<ul>');
-
-            // ====================================
-            // ITERATING THROUGH INGREDIENTS ARRAY  
-            // ====================================
-            //iterating through ingredients array
-            for (var j = 0; j < ingredients.length; j++) {
-               
-            
-
-                // =======================================================================
-                // PREPARING INGREDIENT LIST TO SEARCH FOR PRICES ON WALMART API 
-                // =======================================================================
-                var ingredientName = ingredient.toLocaleLowerCase();
+            console.log('STATEMENT:' + getTotalPrice(recipes[i]));
+            console.log(typeof totalPrice[i]);
 
 
-                // console.log(ingredientName);
-                // console.log(ingredientName.length);
-
-                // use regular expression to remove measurement details from ingredient list
-                var ingredientName = ingredientName.replace(/\d/g, '').replace(/cup/g, '').replace(/\//g, '').replace(/\(ml\)/g, '').replace(/tablespoons/g, '').replace(/-/g, '').replace(/inch/g, '').replace(/pounds/g, '').replace(/\(cm\)/g, '').replace(/\(g\)/g, '').replace(/thawed/g, '').replace(/peeled and grated/g, '').replace(/water/g, '');
-
-                ingredientName = ingredientName.trim();
-                console.log(ingredientName);
-            
-
-                // ============
-                // WALMART API 
-                // ============
-                var apiKey = "gw9az82quh9v3geuvydmm2p6";
-                var queryItem = ingredientName;
-                var queryURL = "https://cors-anywhere.herokuapp.com/https://api.walmartlabs.com/v1/search?query=" + queryItem + "&apiKey=" + apiKey;
-
-                // AJAX CALL
-                $.ajax({
-                    url: queryURL,
-                    method: "GET",
-                }).done(function (results) {
-                    console.log(results);
-                    // items is results.items
-                    var items = results.items;
-
-                    // for loop to iterate through items array 
-                    for (var i = 0; i < 2; i++) {
-                        // items[i] (each item) is an object 
-                        var name = items[i].name;
-                        var price = items[i].salePrice; 
-                        console.log("Ingrident Name: " + name); 
-                        console.log("Price: " + price); 
-                        console.log("URL: " + queryURL); 
-
-                    }
-                });
-
-                ingredient = ingredients[j];
-                var li = $('<li>').html(ingredient + " " + price);
-                ingredientList.append(li);
-            }
-                // post items onto HTML page
-                var recipe_title = $('<h3 class="card-title">').html(title);
-                var recipe_image = $('<img>').attr('src', image_url);
-                var label = $('<h4>').html('Ingredients');
-                //appending those elements to our container div with id of recipes
-                $("#post").append(recipe_title, recipe_image, label, ingredientList);
-
-                console.log(queryURL);
-            
+            console.log("==================================");
+            console.log("This is the Total Price For One Recipe: " + totalPrice[i]);
+            console.log("==================================");
         }
+        // post items onto HTML page
+        var recipe_title = $('<h3 class="card-title">').html(title);
+        var recipe_image = $('<img>').attr('src', image_url);
+        var label = $('<h4>').html('Ingredients');
+        //appending those elements to our container div with id of recipes
+        $("#post").append(recipe_title, recipe_image, label, ingredientList);
+
+        console.log(queryURL);
     });
 });
 
 
 
-// =========================
-// SET UP METHODS: WALMART
-// =========================
-$(".walmartBtn").on("click", function () {
-    console.log("Button Clicked!");
+function getTotalPrice(hitObj) {
+    totalPrice = 0;
+    //recipes[i] (each recipe) is an object
+    var recipe = hitObj.recipe;
+    var title = recipe.label;
+    var image_url = recipe.image;
 
-    // set up api keys 
-    var apiKey = "gw9az82quh9v3geuvydmm2p6";
+    //ingredients is an array
+    var ingredients = recipe.ingredientLines;
+    var ingredientList = $('<ul>');
+    console.log("This is the ingredient List " + JSON.stringify(ingredients));
 
-    var queryItem = "chicken";
+    // ====================================
+    // ITERATING THROUGH INGREDIENTS ARRAY  
+    // ====================================
+    //iterating through ingredients array
+    for (var j = 0; j < ingredients.length; j++) {
 
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.walmartlabs.com/v1/search?query=" + queryItem + "&apiKey=" + apiKey;
+        // =======================================================================
+        // PREPARING INGREDIENT LIST TO SEARCH FOR PRICES ON WALMART API 
+        // =======================================================================
+        ingredient = ingredients[j];
+        var ingredientName = ingredient.toLowerCase();
 
-    var searchURL = "https://api.walmartlabs.com/v1/search?query=" + queryItem + "&apiKey=" + apiKey;
+        // console.log(ingredientName);
+        // console.log(ingredientName.length);
 
+        // use regular expression to remove measurement details from ingredient list
+        var ingredientName = ingredientName.replace(/\d/g, '').replace(/cup/g, '').replace(/\//g, '').replace(/\(ml\)/g, '').replace(/tablespoons/g, '').replace(/-/g, '').replace(/inch/g, '').replace(/pounds/g, '').replace(/\(cm\)/g, '').replace(/\(g\)/g, '').replace(/thawed/g, '').replace(/peeled and grated/g, '').replace(/water/g, '');
 
+        ingredientName = ingredientName.trim();
+        console.log("Trimed Ingredients " + ingredientName);
 
-    // THERE DOESN'T HAVE TO BE WALMART BUTTON  
-    // The Walmart API can be part of the ajax call to Edaman --> or it could be it's separate call and only posted  
-    // next to the ingredient list when module opens 
-    // 
-    // query item needs to come from Edamam API
-    // var queryItem = $(#ingredient).val().trim(); 
-    // need to iterate through each ingredient list for the ajax call 
-    // have to keep scoping in mind: this function doesn't know about the variables in Edamam! 
+        // ============
+        // WALMART API 
+        // ============
+        var apiKey = "gw9az82quh9v3geuvydmm2p6";
+        var queryItem = ingredientName;
+        var queryURL = "https://cors-anywhere.herokuapp.com/https://api.walmartlabs.com/v1/search?query=" + queryItem + "&apiKey=" + apiKey;
 
+        // AJAX CALL
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).done(function (results) {
+            console.log(results);
+            // items is results.items
+            var items = results.items;
 
-    // AJAX CALL
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).done(function (results) {
-        console.log(results);
-        // items is results.items
-        var items = results.items;
+            // fetching first item from the items array
+            var name = items[0].name;
+            var price = items[0].salePrice;
+            console.log("Ingrident Name: " + name);
+            console.log("Price: " + price);
+            console.log("URL: " + queryURL);
 
-        // for loop to iterate through items array 
-        for (var i = 0; i < 3; i++) {
-            // items[i] (each item) is an object 
-            var name = items[i].name;
+            // fetching price of each item and pushing it into an array 
 
-            $("#post").append(name);
-            console.log(results.items);
-            console.log(searchURL);
-        }
-    })
-});
+            if (price !== undefined) {
+                totalPrice += price;
+            }
+            console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            console.log("Local Variable Total Price is: " + totalPrice);
+            console.log(typeof totalPrice);
+            console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
-// 5 Recipes 
-// Display: link that opens module 
-// 4 components: image of recipe, name of dish, total cost (WALMARTS)
+            return totalPrice;
 
-// MODULE
-// ingredient list, price of each ingredient, total price of ingredient, link to recipe, 
+        });
 
-// ----------
-// when click BUY --> takes you to FULL walmart
+        var li = $('<li>').html(ingredient + " " + price);
+        ingredientList.append(li);
+    }
+}
+
+//getTotalPrice();
