00156379dc4cf4c96f51031d1748ae871df7571c
arpitshukla114 <arpitshukla114@yahoo.co.in>
2018-07-15 22:38:30 -0400

    commiting UI changes to branch

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -1,8 +1,37 @@
 $(document).ready(function () {
-    var recipe;
+    var recipe = {
+        image : 'https://images.pexels.com/photos/247685/pexels-photo-247685.png?cs=srgb&dl=food-plate-healthy-247685.jpg&fm=jpg',
+        ingredients : ['ingredients1','ingredients2'],
+        cost : 150
+    };
 
     for (i=0; i < 5; i++) {
-    $(".container").append("<div class=row><p>Title</p></div>");
+        var newRow = $('<div class="row"></div>');
+        var newCol1 = $('<div class="col-2 dish-image"></div>');
+        var newCol2 = $('<div class="col-8"></div>');
+        var newCol3 = $('<div class="col-2 dish-price"></div>');
+        var imagePathHTML = `<img src="${recipe.image}" alt="" height="100%" width="100%" style="height: 90%;width: 90%;">`;
+        newCol1.html(imagePathHTML);
+        newRow.append(newCol1);
+        var listHTML = generateListHTL(recipe.ingredients);
+        newCol2.html(listHTML);
+        newRow.append(newCol2);
+        var costHTML = `<h1>${recipe.cost}</h1>`;
+        newCol3.html(costHTML);
+        newRow.append(newCol3);
+        $(".container").append(newRow);
     }
 
 });
+
+function generateListHTL(ingreList){
+    var listHTML = '<ul>';
+
+    ingreList.forEach(ingre => {
+        listHTML += ('<li>' + ingre + '</li>');
+    });
+
+    listHTML += '</ul>'
+    console.log(listHTML);
+    return listHTML;
+}
